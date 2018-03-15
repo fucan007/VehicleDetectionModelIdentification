69cf54173c479298bed00170dbff928fb87a4898
liuxiaohui <1406744100@qq.com>
2018-03-15 18:05:39 +0800

    build model about vehicle and add configure file about vehicle

--- vehicleDetection/run.py
+++ vehicleDetection/run.py
@@ -5,5 +5,5 @@ w_d = os.path.dirname(os.path.abspath(__file__))
 print('change wording dir to [{0}]'.format(w_d))
 os.chdir(w_d)
 
-for l in os.popen('/bin/bash -c "cd {0} && source ./run.sh"'.format(w_d)):
+for l in os.popen('/bin/bash -c "cd {0} && source ./runVehicle.sh"'.format(w_d)):
     print(l.strip())
